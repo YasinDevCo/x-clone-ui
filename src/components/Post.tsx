68f2f76a2c1b979669f803140c31d08ffb663ec6
yasin abbasspour <yasin.absr83@gmail.com>
2025-10-20 12:46:46 +0330

    make error

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,18 +1,18 @@
-import { imagekit } from "@/utils";
+// import { imagekit } from "@/utils";
 import Image from "./Image";
 import PostInfo from "./PostInfo";
 import PostInteractions from "./PostInteractions";
-import Video from "./Video";
+// import Video from "./Video";
 import Link from "next/link";
 
-interface FileDetailsResponse {
-  width: number;
-  height: number;
-  filePath: string;
-  url: string;
-  fileType: string;
-  customMetadata?: { sensitive: boolean };
-}
+// interface FileDetailsResponse {
+//   width: number;
+//   height: number;
+//   filePath: string;
+//   url: string;
+//   fileType: string;
+//   customMetadata?: { sensitive: boolean };
+// }
 
 const Post = async ({ type }: { type?: "status" | "comment" }) => {
  
